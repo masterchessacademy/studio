12cfb4b305e7c3a187cdd1d137f08537cbd98192
masterchess academy <masterchessacademy16@gmail.com>
2025-10-15 06:56:47 +0000

    Try fixing this error: `Console Error: [2025-10-15T06:55:40.000Z] @fireb

--- src/firebase/index.tsx
+++ src/firebase/index.tsx
@@ -7,7 +7,7 @@ import { createContext, useContext, ReactNode, useMemo } from 'react';
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || undefined,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
